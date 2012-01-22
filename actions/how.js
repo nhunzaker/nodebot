b06a3697fcf4cee038ce8d5dd395d207d5bc7d24
Nathan Hunzaker <nate.hunzaker@gmail.com>
2012-01-21 19:59:50 -0500

    Adds adjustments for 'how'

--- actions/how.js
+++ actions/how.js
@@ -3,6 +3,10 @@
 
 module.exports = function how (a) {
 
+    if (a.owner && a.subject) {
+        this.actions.what.apply(this, [a]);
+    }
+
     this.say("I don't know how to %s", a.subject);
     
     return this.request();
